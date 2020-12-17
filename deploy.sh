2f60810dbe81c314dbfcc6e8df1f78e979f55e81
TZZ <1789803837@qq.com>
2020-12-18 01:48:59 +0800

    更新服务器部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+path="/webdata/"
+log="/webdata/log/blog_page.log"
+
+exec 1>>$log
+exec 2>>$log
+
+date "+%Y-%m-%d %H:%M:%S"
+
+rm -rf $path"public"
+cd $path
+git clone https://e.coding.net/imtzz/Pages.git public
