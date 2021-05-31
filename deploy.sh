4f830637fcc4693841b8b08df3cc56111da8cf35
TZZ <1789803837@qq.com>
2021-05-31 23:56:23 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ path="/webdata/"
 log_path="/webdata/log/"
 log="/webdata/log/blog_page.log"
 
+echo "start deployment"
+
 if [ ! -d "$path" ]; then
     mkdir $path
 fi
@@ -10,6 +12,7 @@ if [ ! -d "$log_path" ]; then
     mkdir $log_path
 fi
 
+echo "set log path"
 touch $log
 
 exec 1>>$log
@@ -17,6 +20,11 @@ exec 2>>$log
 
 date "+%Y-%m-%d %H:%M:%S"
 
+echo `date "+%Y-%m-%d %H:%M:%S"`
+
+echo "delete public dir"
 rm -rf $path"public"
 cd $path
+
+echo "clone pages code."
 git clone git@e.coding.net:imtzz/Pages.git public
